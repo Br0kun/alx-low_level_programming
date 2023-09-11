2137898fce6cb2cae1e8812f02b1d510d6a2dd05
brokxd <brok@gmail.com>
2023-09-11 15:58:34 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 
 /**
- * main - program prints a line with puts function
- * Return: 0 (success)
+ * main - Entry point of the program
+ *
+ * Description: This program prints a line using the puts function.
+ *
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
+
