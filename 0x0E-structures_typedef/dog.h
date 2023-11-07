1cca7596bd6cb0ee09b1eb9c590188d3860f26ce
br0k <ilyasdh@gmail.com>
2023-11-06 18:03:20 -0800

    dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,7 +7,7 @@
  * @age: dog's age
  * @owner: owner's name
  *
- * Description:just a lone dog struct in a big kitty world
+ * This structure defines the basic information about a dog,
  */
 struct dog
 {
